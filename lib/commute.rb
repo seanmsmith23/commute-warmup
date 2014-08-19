60ed314d307daa9b49b492e3adae12589401010f
Sean Smith <seansmith1020@gmail.com>
2014-08-19 11:02:39 -0600

    fixed walking example

--- lib/commute.rb
+++ lib/commute.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'awesome_print'
 
 table = CSV.read('/Users/seansmith/gSchoolWork/warmups/commute-warm-up/data/gschool_commute_data.csv', headers: true)
 
@@ -14,8 +15,10 @@ data.each do |k,v|
   end
 end
 
+ap data
+
 data["Nate"].each do |row|
-  p "Nate's inbuund commute on Week 4 Wednesday was #{row["Inbound"].to_i} minutes" if row["Week"] == "4" && row["Day"] == "Wednesday"
+  p "Nate's inbound commute on Week 4 Wednesday was #{row["Inbound"].to_i} minutes" if row["Week"] == "4" && row["Day"] == "Wednesday"
 end
 
 total = 0
@@ -32,16 +35,27 @@ fastest = []
 
 data.each do |k,v|
   time = 0
+  divide_by = 0
   v.each do |row|
-    time += (row["Inbound"].to_i + row["Outbound"].to_i)
+    if row["Mode"] == "Walk"
+      time += (row["Inbound"].to_i + row["Outbound"].to_i)
+      divide_by += 1
+    end
   end
-  fastest << [k, time/20]
+  fastest << [k, time/(divide_by > 0 ? divide_by : 1)]
 end
+
+fastest.delete_if { |result| result[1] == 0 }
+
 
 who_won = fastest.sort_by{|times| times[1]}
 
-p "The shortest commute was #{who_won[0][0]}, her average commute was: #{who_won[0][1]} minutes."
+p "The shortest walking commute was #{who_won[0][0]}, her average commute was: #{who_won[0][1]} minutes."
 
+data.each do |k,v|
+  v.sort_by { |row| row["Week"] }
+end
 
+# ap data
 
 
