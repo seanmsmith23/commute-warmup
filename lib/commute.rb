ae33b57f4bf3a09d452b5d80a29f96d48583e8c0
Sean Smith <seansmith1020@gmail.com>
2014-08-19 11:50:16 -0600

    sorting is done appropriately (week then day)

--- lib/commute.rb
+++ lib/commute.rb
@@ -1,13 +1,28 @@
 require 'csv'
 require 'awesome_print'
 
+
 table = CSV.read('/Users/seansmith/gSchoolWork/warmups/commute-warm-up/data/gschool_commute_data.csv', headers: true)
 
-new = table.map do |row|
+hashes = table.map do |row|
   row.to_hash
 end
 
+days = {"Monday" => 1, "Tuesday" => 2, "Wednesday" => 3, "Thursday" => 4, "Friday" => 5}
+
+new = hashes.sort do |a, b|
+  if (a["Week"] <=> b["Week"]) == 0
+    a = a["Day"]
+    b = b["Day"]
+    days[a] <=> days[b]
+  else
+    a["Week"] <=> b["Week"]
+  end
+end
+
 data =  new.group_by { |row| row["Person"] }
+
+ap data
 
 data.each do |k,v|
   v.each do |row|
@@ -15,7 +30,10 @@ data.each do |k,v|
   end
 end
 
-ap data
+
+new = data.each do |k,v|
+  v
+end
 
 data["Nate"].each do |row|
   p "Nate's inbound commute on Week 4 Wednesday was #{row["Inbound"].to_i} minutes" if row["Week"] == "4" && row["Day"] == "Wednesday"
@@ -47,15 +65,6 @@ end
 
 fastest.delete_if { |result| result[1] == 0 }
 
-
 who_won = fastest.sort_by{|times| times[1]}
 
 p "The shortest walking commute was #{who_won[0][0]}, her average commute was: #{who_won[0][1]} minutes."
-
-data.each do |k,v|
-  v.sort_by { |row| row["Week"] }
-end
-
-# ap data
-
-
